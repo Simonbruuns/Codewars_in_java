b58a82d406ee4fcfc685691ea8b786ab2b295502
SimonBruun <bsb.bruun@hotmail.com>
2023-07-10 13:33:31 +0200

    Just commenting

--- src/main/java/Codewars/Arge.java
+++ src/main/java/Codewars/Arge.java
@@ -1,5 +1,5 @@
 package Codewars;
-
+//Mispelled, supposed to be age. To lazy to edit it.
 public class Arge {
     public static int nbYear(int p0, double percent, int aug, int p) {
         int years = 0;
@@ -14,7 +14,7 @@ public class Arge {
     }
 
 
-
+//Just primitive testing of the method.
     public static void main(String[] args){
         System.out.println(nbYear(1000,2,50,1200));
     }
--- src/main/java/Scaler/calculating.java
+++ src/main/java/Scaler/calculating.java
@@ -5,7 +5,7 @@ public class calculating {
 
     public static int remainder(int num1, int num2, int num3){
         int sum = num1 + num2;
-        return sum % num3;
+            return sum % num3;
     }
     public static void main(String[] args) {
 
