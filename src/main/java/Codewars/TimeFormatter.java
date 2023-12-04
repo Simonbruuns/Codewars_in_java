dd7dd3e07d6946552ee265ee86a05cedcf4f0588
SimonBruun <bsb.bruun@hotmail.com>
2023-12-04 20:52:15 +0100

    TimeFormatter done

--- src/main/java/Codewars/TimeFormatter.java
+++ src/main/java/Codewars/TimeFormatter.java
@@ -1,43 +1,62 @@
 package Codewars;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TimeFormatter {
     public static String formatDuration(int seconds) {
-        //minutes calculated
-        int M = seconds / 60;
-        //hours calculated
-        int H = seconds / 3600;
-        //Remaining seconds
-        int RS = seconds % 60;
-        int RM = M % 60;
-      if (seconds < 60) {
-            return seconds + (seconds == 1 ? " second" : " seconds");
-        }
-      else if (seconds >= 60 && seconds < 3600) {
-            String MS = M + (M == 1 ? " minute" : " minutes");
-            String RSS = RS + (RS == 1 ? " second" : " seconds");
-            if(RS == 0)
-                return MS;
-            else
-            return MS + " and " + RSS;
-        }
-      else if (seconds >= 3600) {
-            //ternary conditional operators/shortere if statements.
-          // Checking whether it should be plural or not.
-            String HS = H + (H == 1 ? " hour" : " hours");
-            String MS = RM + (RM == 1 ? " minute" : " minutes");
-            String RSS = RS + (RS == 1 ? " second" : " seconds");
-            if(RM == 0 && RS == 0)
-                return HS;
-            else
-                return HS + ", " + MS + " and "+ RSS;
-        } else if (seconds >=86400) {
+        // Calculate the number of years, days, and remaining seconds
+        int year = seconds / 31536000;  // 365 * 24 * 60 * 60
+        int remainingSecondsInYear = seconds % 31536000;
 
-          
-      }
-        return "now";
+        // Calculate the number of days and remaining seconds in a day
+        int day = remainingSecondsInYear / 86400;
+        int remainingSecondsInDay = remainingSecondsInYear % 86400;
+
+        // Calculate hours, minutes, and seconds
+        int H = remainingSecondsInDay / 3600;
+        int M = (remainingSecondsInDay % 3600) / 60;
+        int RS = remainingSecondsInDay % 60;
+
+        // Ternary conditional operators/shorter if statements.
+        // Checking whether it should be plural or not.
+        String YS = year + (year == 1 ? " year" : " years");
+        String DS = day + (day == 1 ? " day" : (day > 0 ? " days" : ""));
+        String RHS = H + (H == 1 ? " hour" : (H > 0 ? " hours" : ""));
+        String MS = M + (M == 1 ? " minute" : (M > 0 ? " minutes" : ""));
+        String RSS = RS + (RS == 1 ? " second" : (RS > 0 ? " seconds" : ""));
+
+        // Construct the result list
+        List<String> components = new ArrayList<>();
+        if (year > 0) {
+            components.add(YS);
+        }
+        if (day > 0) {
+            components.add(DS);
+        }
+        if (H > 0) {
+            components.add(RHS);
+        }
+        if (M > 0) {
+            components.add(MS);
+        }
+        if (RS > 0) {
+            components.add(RSS);
+        }
+
+        // Handle "now" case
+        if (components.isEmpty()) {
+            return "now";
+        }
+
+        // Join the components with commas and "and" as needed
+        String result = String.join(", ", components.subList(0, components.size() - 1))
+                + (components.size() > 1 ? " and " : "")
+                + components.get(components.size() - 1);
+
+        return result;
     }
-
     public static void main(String[] args){
-        System.out.println(formatDuration(3788));
+        System.out.println(formatDuration(86400120));
     }
 }
